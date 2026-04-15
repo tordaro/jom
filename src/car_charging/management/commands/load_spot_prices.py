c9ad1ab12a98a0cd40579b3b7093b66904632151
Tord Aronsen <tord.aronsen@skatteetaten.no>
2026-04-15 13:19:07 +0200

    correct help text

--- src/car_charging/management/commands/load_spot_prices.py
+++ src/car_charging/management/commands/load_spot_prices.py
@@ -11,7 +11,7 @@ class Command(BaseCommand):
     help = "Load spot price data from https://www.forbrukerradet.no/strompris/spotpriser."
 
     def add_arguments(self, parser: CommandParser) -> None:
-        parser.add_argument("file_path", type=Path, help="Price area.")
+        parser.add_argument("file_path", type=Path, help="Path to the Excel file containing spot price data.")
 
     def handle(self, *args, **options) -> None:
         load_spot_prices(options["file_path"])
