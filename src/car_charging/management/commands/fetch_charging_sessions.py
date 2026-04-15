d94c00ed154a09373e0e60266883f83260219cb1
Tord Aronsen <tord.aronsen@skatteetaten.no>
2026-04-15 13:20:33 +0200

    tighten fetch sessions command

--- src/car_charging/management/commands/fetch_charging_sessions.py
+++ src/car_charging/management/commands/fetch_charging_sessions.py
@@ -1,6 +1,7 @@
 import logging
+from datetime import timedelta
 from django.utils.timezone import datetime, make_aware
-from django.core.management.base import BaseCommand, CommandParser
+from django.core.management.base import BaseCommand, CommandError, CommandParser
 
 import car_charging.zaptec_services as zts
 
@@ -15,11 +16,11 @@ class Command(BaseCommand):
         parser.add_argument("end", type=lambda d: datetime.strptime(d, "%d-%m-%Y"), help="End date, inclusive (dd-mm-YYYY).")
 
     def handle(self, *args, **options) -> None:
-        start_date = make_aware(options["start"]).date()
-        end_date = make_aware(options["end"]).date()
+        start_date = make_aware(options["start"])
+        end_date_exclusive = make_aware(options["end"]) + timedelta(days=1)
 
-        if end_date < start_date:
-            print("End date must be later than or equal to start date.")
-        else:
-            charging_data = zts.get_charge_history_data(start_date, end_date)
-            _ = zts.create_charging_sessions(charging_data)
+        if end_date_exclusive <= start_date:
+            raise CommandError("End date must be later than or equal to start date.")
+
+        charging_data = zts.get_charge_history_data(start_date, end_date_exclusive)
+        _ = zts.create_charging_sessions(charging_data)
